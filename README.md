b23be27690daff997f8b90dd108e4a6fedeb5015
valentin <crozet.valentin.42@gmail.com>
2020-04-06 21:55:50 +0200

    maj

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mongo Database
+
+## Launch Database
+
+
+```docker-compose up -d```
+
+## Mongo URI
+
+```MONGODB_URI=mongodb://user-test:user-test@localhost:27017/ermes_api```
--- init-mongo.js
+++ init-mongo.js
@@ -1,3 +1,4 @@
+print('Start #################################################################');
 db.createUser(
   {
     user: "admin",
@@ -10,223 +11,50 @@ db.createUser(
     ]
   }
 )
-db.createUser(
+db1 = db.getSiblingDB('ermes_api')
+db1.createUser(
   {
-    user: "ermes_api_funnel",
-    pwd: "ermes_api_funnel",
+    user: "user-test",
+    pwd: "user-test",
     roles: [
       {
         role: "readWrite",
-        db: "ermes_funnel"
+        db: "ermes_api"
       }
     ]
   }
 )
-db.createUser(
+db1.createCollection('funnels')
+db1.createCollection('funnelconnectors')
+db1.createCollection('funneldatacollections')
+db1.createCollection('funnelconnectortimeseries')
+db1.createCollection('funnelanomalies')
+db1.createCollection('funneldatatimeseries')
+db1.createCollection('funnelcontributions')
+db1.createCollection('funnelleverages')
+db1.createCollection('funneltimeseries')
+db1.createCollection('funnelsyntheses')
+db1.createCollection('funnelmediadistribs')
+db1.createCollection('users')
+db1.createCollection('groups')
+db1.createCollection('category')
+db1.createCollection('criteria')
+
+db2 = db.getSiblingDB('ermes_ds')
+db2.createUser(
   {
-    user: "user_testing",
-    pwd: "password_testing",
+    user: "user-test",
+    pwd: "user-test",
     roles: [
       {
         role: "readWrite",
-        db: "db_testing"
+        db: "ermes_ds"
       }
     ]
   }
 )
-db.getCollection('funnels').insertOne({
-  _id: ObjectId("5ddbcb572da64897712b44ce"),
-  userId: ObjectId("5ddbcb6669035097d3d8a058"),
-  name: 'funnel_test',
-  saved: true,
-  start: '2019-08-01T21:59:59.999Z',
-  end: '2019-04-01T21:59:59.999Z',
-  connected: [
-    {
-      "funnelId": "CPM",
-      "connector": {
-        "id": 6,
-        "name": "Google Play",
-        "src": "/assets/img/connector/google_play.png",
-        "sso": true
-      },
-      "data": {},
-      "fetched": true
-    },
-    {
-      "funnelId": "CPV",
-      "connector": {
-        "id": 6,
-        "name": "Google Play",
-        "src": "/assets/img/connector/google_play.png",
-        "sso": true
-      },
-      "data": {},
-      "childId": [
-        "CPE",
-        "CPO",
-        "CPOHB"
-      ],
-      "fetched": true
-    },
-    {
-      "funnelId": "CPOHB",
-      "connector": {
-        "id": 6,
-        "name": "Google Play",
-        "src": "/assets/img/connector/google_play.png",
-        "sso": true
-      },
-      "data": {},
-      "fetched": true
-    },
-    {
-      "funnelId": "CPE",
-      "connector": {
-        "id": 6,
-        "name": "Google Play",
-        "src": "/assets/img/connector/google_play.png",
-        "sso": true
-      },
-      "data": {},
-      "fetched": true
-    },
-    {
-      "funnelId": "CPO",
-      "connector": {
-        "id": 6,
-        "name": "Google Play",
-        "src": "/assets/img/connector/google_play.png",
-        "sso": true
-      },
-      "data": {},
-      "fetched": true
-    }
-  ],
-  objectifed: [
-    {
-      "funnelId": "CPM",
-      "value": "1.00"
-    },
-    {
-      "funnelId": "CPV",
-      "value": "2.00"
-    },
-    {
-      "funnelId": "CPE",
-      "value": "3.00"
-    },
-    {
-      "funnelId": "CPO",
-      "value": "4.00"
-    }
-  ],
-  measure: {
-    "name": "Leads",
-    "description": "Analysez l’impact de vos campagnes médias sur vos nouveaux leads et augmentez votre taux d’acquisition",
-    "preview": "/static/img/preview/leads-preview.png",
-    "details": [
-      "Impressions",
-      "Visites Homepage",
-      "Engagements",
-      "Offres"
-    ],
-    "baseline": {
-      "wording": {
-        "total": "Offres au total",
-        "advertising_title": "Nombre d'offres provenant de la campagne.",
-        "advertising_description": "(Ce sont les nouvelles offres obtenues grâce à votre campagne média)",
-        "baseline_title": "Nombre d'offres provenant du trafic natuel.",
-        "baseline_description": "(C'est le nombre de nouvelles offres que vous auriez eu sans campagne média)",
-        "kpi_title": "CPO hors baseline.",
-        "pki_description": "(Correspond au coût pour chaque nouvelle offre provenant de la campagne uniquement)"
-      }
-    },
-    "funnels": [
-      {
-        "id": "CPM",
-        "name": "Impressions publicitaires ciblées",
-        "title": "CPM",
-        "per": 1000,
-        "cr": false,
-        "average": 2.6,
-        "connectors": [
-          1,
-          2,
-          3,
-          4,
-          12,
-          22,
-          23,
-          5],
-        "button": [
-          "Profils"
-        ]
-      },
-      {
-        "id": "CPV",
-        "name": "Visites Homepage",
-        "title": "Taux de visite",
-        "average": 0.75,
-        "per": 1,
-        "target": true,
-        "childId": [
-          "CPE",
-          "CPO",
-          "CPOHB"
-        ],
-        "forecast": [
-          "FCPO",
-          "FACPO"
-        ],
-        "connectors": [
-          11,
-          13,
-          5
-        ],
-        "button": [
-          "Profils",
-          "Look Alike",
-          "Retargeting"
-        ]
-      },
-      {
-        "id": "CPE",
-        "name": "Engagements",
-        "title": "Taux d'engagement",
-        "average": 87,
-        "per": 1,
-        "connectors": [],
-        "button": [
-          "Profils",
-          "Look Alike",
-          "Retargeting"
-        ]
-      },
-      {
-        "id": "CPO",
-        "name": "Offres",
-        "title": "Taux d'offres",
-        "average": 60,
-        "per": 1,
-        "connectors": [],
-        "button": [
-          "Profils",
-          "Look Alike",
-          "Retargeting"
-        ]
-      },
-      {
-        "id": "CPOHB",
-        "name": "Baseline",
-        "title": "Baseline",
-        "average": 60,
-        "per": 1,
-        "connectors": [],
-        "cr": false,
-        "objectifable": false,
-        "baseline": "CPO"
-      }
-    ]
-  }
-})
+db2.createCollection('funnelTrends')
+db2.createCollection('funnelSectors')
+db2.createCollection('funnelSeasonality')
+
+print('End #################################################################');
